6dcc2684153109069d6da0c579407a8ab9fd6400
German Pinto <93947560+germanp007@users.noreply.github.com>
2022-02-08 12:09:29 -0300

    Add files via upload

---
+++ calculadora2/calculator.js
@@ -0,0 +1,39 @@
+window.addEventListener("load", () => {
+  const display = document.querySelector(".calculator-display");
+  const keypadButtons = document.getElementsByClassName("keypadbutton");
+
+  const keypadButtonsArray = Array.from(keypadButtons);
+
+  keypadButtonsArray.forEach((button) => {
+    button.addEventListener("click", () => {
+      calculadora(button, display);
+    });
+  });
+});
+
+function calculadora(button, display) {
+  switch (button.innerHTML) {
+    case "C":
+      borrar(display);
+      break;
+    case "=":
+      calcular(display);
+      break;
+    default:
+      actualizar(display, button);
+  }
+}
+
+function calcular(display) {
+  display.innerHTML = eval(display.innerHTML);
+}
+function actualizar(display, button) {
+  if (display.innerHTML == 0) {
+    display.innerHTML = "";
+  }
+  display.innerHTML += button.innerHTML;
+}
+
+function borrar(display) {
+  display.innerHTML = 0;
+}
